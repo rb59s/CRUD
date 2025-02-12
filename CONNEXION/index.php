579c2135d6f3d1fc684a0032832edb87fa9d4143
rb59ls <berrazzag59dbz@gmail.com>
2025-02-12 14:54:28 +0100

    absenczs

---
+++ CONNEXION/index.php
@@ -0,0 +1,16 @@
+<?php $contenu = <<<HTML
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+
+</head>
+
+
+
+
+HTML;
+echo $contenu; ?>
